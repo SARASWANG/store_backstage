0e444e8a76fa1918a563eda9b11e35851db2925b
wangqiao <2337523303@qq.com>
2018-07-08 21:30:43 +0800

    搜索框+添加按钮

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -7,15 +7,34 @@
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 搜索区域 -->
+    <el-row>
+      <el-col :span="24">
+        <el-input clearable placeholder="请输入内容" v-model="formData" class="input-with-select searchlist">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formData: ''
+    };
+  }
+};
 </script>
 
 <style>
 .box-card {
   height: 100%;
 }
+.el-input.searchlist {
+    width: 380px;
+    margin: 20px 0;
+  }
 </style>
