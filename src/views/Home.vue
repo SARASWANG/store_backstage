bdf98d16c9b0fa7d7d0e5811daf4418b3b6f18ce
wangqiao <2337523303@qq.com>
2018-07-08 15:15:00 +0800

    头部header完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <img src="" alt="">
+        </el-col>
+        <el-col :span="19">
+          <h2 class="middle">电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container class="container">
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -19,6 +31,12 @@ export default {};
 
 .header {
   background-color: #b3c0d1;
+  padding: 0;
+}
+.header .middle,.header .logout {
+  text-align: center;
+  line-height: 60px;
+  color: #fff;
 }
 
 .aside {
