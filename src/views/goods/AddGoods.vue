1f76bf3721b75d8ac041d317307a5a1aebc238e1
wangqiao <2337523303@qq.com>
2018-07-14 22:46:49 +0800

    goods---步骤条和标签页组件1

--- src/views/goods/AddGoods.vue
+++ src/views/goods/AddGoods.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>addgoods</div>
-</template>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <my-breadcrumb level1="商品管理" level2="商品分类"></my-breadcrumb>
+    <!-- 步骤条 -->
+    <el-steps :active="1" finish-status="success" align-center>
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
+    </el-steps>
+    <!-- /步骤条 -->
 
+    <!-- 标签页 -->
+    <el-tabs tab-position="left" style="height: 200px;">
+      <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+    </el-tabs>
+    <!-- /标签页 -->
+  </el-card>
+</template>
 <script>
 export default {
 
